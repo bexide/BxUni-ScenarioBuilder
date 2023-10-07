0be47673b8aa2fdd9c9d08d592c79bee99cefe7e
Kochoyume <78918084+kochounoyume@users.noreply.github.com>
2023-10-07 23:48:17 +0900

    テキストではなくアイコンでボタンを表示するための属性の下準備

--- Packages/BxUni-ScenarioBuilder/Editor/Scripts/Attribute/CommandToolKitEditorMenuItemAttribute.cs
+++ Packages/BxUni-ScenarioBuilder/Editor/Scripts/Attribute/CommandToolKitEditorMenuItemAttribute.cs
@@ -26,4 +26,28 @@ namespace BxUni.ScenarioBuilder.Editor
             Priority     = priority;
         }
     }
+
+    /// <summary>
+    /// BxUni-ScenarioBuilderウィンドウのメニューアイテムをアイコンで追加するためのアトリビュート
+    /// <para>※ static なメソッドに実装してください</para>
+    /// </summary>
+    [System.AttributeUsage(System.AttributeTargets.Method)]
+    public sealed class ScenarioBuilderEditorMenuItemIconAttribute : System.Attribute
+    {
+        internal string MenuItemIconName { get; }
+
+        internal int Priority { get; }
+
+        /// <summary>
+        /// BxUni-ScenarioBuilderウィンドウのメニューアイテムをアイコンで追加するためのアトリビュート
+        /// <para>※ static なメソッドに実装してください</para>
+        /// </summary>
+        /// <param name="menuItemIconName">メニューアイコン名</param>
+        /// <param name="priority">表示優先度</param>
+        public ScenarioBuilderEditorMenuItemIconAttribute(string menuItemIconName, int priority = 0)
+        {
+            MenuItemIconName = menuItemIconName;
+            Priority     = priority;
+        }
+    }
 }
